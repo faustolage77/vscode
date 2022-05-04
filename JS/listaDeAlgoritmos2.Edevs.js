d11b2895d18372f6b02c71c6c9c03e1705cbf922
faustolage77 <faustohenrique.prado@gmail.com>
2022-05-04 15:14:04 -0300

    DEVS_Lista_de_Exercicios_2_Tarefa05.05.2022

--- JS/listaDeAlgoritmos2.Edevs.js
+++ JS/listaDeAlgoritmos2.Edevs.js
@@ -1,50 +1,46 @@
-/*  1########### 
-Criar a função (inverter) que recebe uma string e retorna um inverso dela “salve” => “evlas”
+/*
+1) Criar a função (inverter) que recebe uma string e retorna um inverso dela “salve” => “evlas”
 
-2########## 
-Criar a função (contarNumeroVogais) que recebe uma string e retorna o número de vogais que ela tem
+2) Criar a função (contarNumeroVogais) que recebe uma string e retorna o número de vogais que ela tem
  (maiusculas e minusculas) “salve” => 2
 
-
-
-3############
- Extra: Criar a função (fazerRelatorio) que recebe
+Extra: Criar a função (fazerRelatorio) que recebe
  uma string e retorna um objeto com a própria string, o inverso da string e quantas vogais 
  ela tem “salve” => { palavra: “salve”, palavraInverso: “evlas”, numeroVogais: 2 }
 
-4############
-
-xtra2: Se receber algo diferente de string da um console.log(“oh carai”) e retorna undefined
+Extra2: Se receber algo diferente de string da um console.log(“oh carai”) e retorna undefined
 */
 
-// ###################################################################################
-
-
-
-// 1 
 
 // Criar a função (inverter) que recebe uma string e retorna um inverso dela “salve” => “evlas”
 
-// function inverter(){
-//     let palavra = window.prompt('Me diga uma palavra: ');  
-//     console.log(palavra);
+/*
+function inverter(){
+    let palavra = 'Desenvolvimento';  
+    console.log(palavra);
 
-//     let stringParaArray = palavra.split('');  
-
-
-//     palavraInvertida = stringParaArray.reverse()  
-
-//     console.log(palavraInvertida.join('')) 
-//     alert(palavraInvertida.join(''))
-// }; 
+    let stringParaArray = palavra.split('');  
 
 
-// inverter()  
+    palavraInvertida = stringParaArray.reverse()  
 
-////////////////////////////////
+    console.log(palavraInvertida.join('')) 
+  
+}; 
+
+inverter()  
+*/
+
+
+
+//########
+
+
 
 /*Criar a função (contarNumeroVogais) que recebe uma string e retorna o número de vogais que ela tem
  (maiusculas e minusculas) “salve” => 2 */
+
+ /*
 
  function fazerRelatorio(Desenvolvimento) {
     const tamanho = Desenvolvimento.length;
@@ -95,3 +91,5 @@ xtra2: Se receber algo diferente de string da um console.log(“oh carai”) e r
     }
   }
   fazerRelatorio('Fausto');
+
+  */
