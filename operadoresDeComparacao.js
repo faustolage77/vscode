7a9e6d266fc8d8b22748ce2d14427ae14fb525bd
Fausto Lage <100029945+faustolage77@users.noreply.github.com>
2022-05-26 15:53:28 -0300

    Add files via upload

---
+++ operadoresDeComparacao.js
@@ -0,0 +1,23 @@
+/*
+Operadores de comapracao;
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor)
+=== igualdade estrita (checa valor e tipo)
+!=
+!--
+
+
+*/
+
+// const comparacao = 10 > 5; 
+
+// console.log(comparacao);
+
+// const num1 = 10;
+// const num2 = '10';
+// const comparacao = num1 !== num2; // USAR COM 3 ===
+
+// console.log(comparacao);
